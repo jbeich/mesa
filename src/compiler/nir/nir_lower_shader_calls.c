d58b934a6aa70d862a86923ef89a213e08440c1d
Jan Beich <jbeich@FreeBSD.org>
2020-11-25 22:05:56 +0000

    nir: rename struct bitset on FreeBSD after 8dfb240b1f06
    src/compiler/nir/nir_lower_shader_calls.c:81:8: error: redefinition of 'bitset'
    struct bitset {
           ^
    /usr/include/sys/_bitset.h:64:15: note: previous definition is here
    BITSET_DEFINE(bitset, 1);
                  ^
    src/compiler/nir/nir_lower_shader_calls.c:90:8: error: field designator 'set' does not refer to any field in type 'struct bitset'
          .set = rzalloc_array(mem_ctx, BITSET_WORD, BITSET_WORDS(size)),
           ^
    src/compiler/nir/nir_lower_shader_calls.c:91:8: error: field designator 'size' does not refer to any field in type 'struct bitset'
          .size = size,
           ^
    src/compiler/nir/nir_lower_shader_calls.c:106:32: error: no member named 'size' in 'struct bitset'
       if (src->ssa->index >= set->size)
                              ~~~  ^
    src/compiler/nir/nir_lower_shader_calls.c:109:28: error: no member named 'set' in 'struct bitset'
       return BITSET_TEST(set->set, src->ssa->index);
                          ~~~  ^
    src/util/bitset.h:61:30: note: expanded from macro 'BITSET_TEST'
     #define BITSET_TEST(x, b) (((x)[BITSET_BITWORD(b)] & BITSET_BIT(b)) != 0)
                                  ^
    src/compiler/nir/nir_lower_shader_calls.c:115:27: error: no member named 'size' in 'struct bitset'
       if (def->index >= set->size)
                         ~~~  ^
    src/compiler/nir/nir_lower_shader_calls.c:118:20: error: no member named 'set' in 'struct bitset'
       BITSET_SET(set->set, def->index);
                  ~~~  ^
    src/util/bitset.h:62:28: note: expanded from macro 'BITSET_SET'
     #define BITSET_SET(x, b) ((x)[BITSET_BITWORD(b)] |= BITSET_BIT(b))
                                ^
    src/compiler/nir/nir_lower_shader_calls.c:396:23: error: no member named 'set' in 'struct bitset'
             memcpy(remat.set, trivial_remat.set, live_words * sizeof(BITSET_WORD));
                    ~~~~~ ^
    src/compiler/nir/nir_lower_shader_calls.c:396:42: error: no member named 'set' in 'struct bitset'
             memcpy(remat.set, trivial_remat.set, live_words * sizeof(BITSET_WORD));
                               ~~~~~~~~~~~~~ ^
    src/compiler/nir/nir_lower_shader_calls.c:406:63: error: no member named 'set' in 'struct bitset'
                BITSET_WORD spill_mask = live[w] & ~trivial_remat.set[w];
                                                    ~~~~~~~~~~~~~ ^
    src/compiler/nir/nir_lower_shader_calls.c:443:33: error: no member named 'set' in 'struct bitset'
                   BITSET_SET(remat.set, index);
                              ~~~~~ ^
    src/util/bitset.h:62:28: note: expanded from macro 'BITSET_SET'
     #define BITSET_SET(x, b) ((x)[BITSET_BITWORD(b)] |= BITSET_BIT(b))
                                ^
    src/compiler/nir/nir_lower_shader_calls.c:827:34: error: no member named 'set' in 'struct bitset'
                   BITSET_SET(remat->set, def->index);
                              ~~~~~  ^
    src/util/bitset.h:62:28: note: expanded from macro 'BITSET_SET'
     #define BITSET_SET(x, b) ((x)[BITSET_BITWORD(b)] |= BITSET_BIT(b))
                                ^
    12 errors generated.

--- src/compiler/nir/nir_lower_shader_calls.c
+++ src/compiler/nir/nir_lower_shader_calls.c
@@ -78,6 +78,12 @@ instr_is_shader_call(nir_instr *instr)
           intrin->intrinsic == nir_intrinsic_execute_callable;
 }
 
+#ifdef __FreeBSD__
+/* nir_builder.h -> half_float.h -> u_thread.h -> pthread_np.h -> sys/cpuset.h -> sys/_bitset.h
+ * - defines `struct bitset` which clashes with the following
+ */
+#define bitset mesa_bitset
+#endif
 struct bitset {
    BITSET_WORD *set;
    unsigned size;
